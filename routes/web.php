20e319a78dd6fea9eced8f96823c73f403c02fcc
adearya <ade.aryabimantara@gmail.com>
2023-11-25 20:51:59 +0700

    On branch new-update  Changes to be committed: 	modified:   resources/views/layouts/landing_page.blade.php 	modified:   resources/views/partials/landing_page_contents/about.blade.php 	modified:   resources/views/partials/landing_page_contents/home.blade.php 	modified:   resources/views/partials/landing_page_contents/search_collection.blade.php 	modified:   resources/views/partials/landing_page_contents/statistic.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Models\Post;
-use App\Models\Category;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\LoginController;
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
@@ -15,26 +15,26 @@ Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/signup', [SignupController::class, 'index']);
 Route::post('/signup', [SignupController::class, 'store']);
 
-// Landing Pages
+// Landing Page
 Route::get('/', function () {    
     $latestPosts = Post::latest()->take(4)->get(['title', 'views_count', 'slug']);
 
     return view('landing_pages.index', [
         'posts' => $latestPosts,
     ]);
-    })->name('index');
+    })->name('landing_page');
 
-// Get Posts
+// Dashboard
 Route::get('/posts', [PostController::class, 'index'])->name('all_posts')->middleware('auth');
 
-// Get Post
-Route::get('/post/{slug}', [PostController::class, 'show'])->name('single_post');
-
-// Categories
-// Route::get('/categories', [CategoryController::class, 'index'])->name('all_categories');
+// Detail Page
+Route::get('/posts/{slug}', [PostController::class, 'show'])->name('single_post');
 
 // Posts by Category
 Route::get('/category/{category:slug}', [CategoryController::class, 'index'])->name('single_category');
 
 // Posts by Author
 Route::get('/author/{author:username}', [AuthorController::class, 'index'])->name('posts_auhtor');
+
+// Categories
+// Route::get('/categories', [CategoryController::class, 'index'])->name('all_categories');
